a37a59272cade9d334c9925a4ef6e484235a74a4
Abdellah Ennajari <105159582+ennajari@users.noreply.github.com>
2024-06-25 18:33:20 +0100

    Update Ouvrier.java

--- Ouvrier.java
+++ Ouvrier.java
@@ -2,7 +2,6 @@ import java.util.Date;
 
 public class Ouvrier extends Empolye {
     private Date Date_Entree;
-
     public Ouvrier(String Matricule, String Nom, String Prenom, Date Date_N, Date Date_Entree) {
         super(Matricule, Nom, Prenom, Date_N);
         this.Date_Entree = Date_Entree;
