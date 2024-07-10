1898b0f5ba92bd2cd3e8b982c2964e602b177305
Abdellah Ennajari <105159582+ennajari@users.noreply.github.com>
2024-07-10 01:17:36 +0100

    Update Principale.java

--- Principale.java
+++ Principale.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 public class Principale {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-// Saisie des données pour un ouvr
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");// Saisie des données pour un ouvr
         System.out.println("Saisie des données pour un ouvrier:");
         System.out.print("Matricule: ");
         String matriculeOuvrier = scanner.nextLine();
